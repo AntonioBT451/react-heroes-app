d2ac43fd2a9225eb2dd93b4c5da5bfe1bfc76673
AntonioBT451 <antoniobt451@gmail.com>
2026-05-07 07:39:19 -0600

    initial commit: React + TypeScript + TailwindCSS + Shadcn setup

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div className='bg-amber-200'>Hola mundo</div>
+  </StrictMode>,
+)
