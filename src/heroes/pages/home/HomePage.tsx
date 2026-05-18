9c1f0a9324bcbe65ce7a22bc7771ead6088e8e07
AntonioBT451 <antoniobt451@gmail.com>
2026-05-18 09:50:12 -0600

    feat: add home page with tab navigation, stats dashboard, and pagination

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -1,7 +1,109 @@
 // src/heroes/pages/home/HomePage.tsx
 
+import {
+    Heart,
+} from "lucide-react"
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { CustomJumbotron } from '../../../components/custom/CustomJumbotron';
+import { HeroStats } from "@/heroes/components/HeroStats"
+import { HeroGrid } from "@/heroes/components/HeroGrid"
+import { useState } from "react";
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
+
 export const HomePage = () => {
+
+    const [activeTab, setActiveTab] = useState<
+        'all' |
+        'favorites' |
+        'heroes' |
+        'villains'
+    >('all');
+
     return (
-        <div>HomePage</div>
+        <>
+            <>
+                {/* Header */}
+                <CustomJumbotron
+                    title={"Universo de Superheroes"}
+                    description={"Descubre, explora y administra tus superheroes y villanos favoritos."}
+                />
+
+                <CustomBreadcrumbs currentPage={"Super Héroes"} breadcrumbs={[
+                    // {
+                    //     label: "Page01",
+                    //     to: "/"
+                    // },
+                    // {
+                    //     label: "Page02",
+                    //     to: "/"
+                    // },
+                ]} />
+
+                {/* Stats Dashboard */}
+                <HeroStats />
+
+                {/* Tabs */}
+                <Tabs value={activeTab} className="mb-8">
+                    <TabsList className="grid w-full grid-cols-4">
+                        <TabsTrigger value="all"
+                            onClick={() => setActiveTab('all')}
+                        >
+                            All Characters (16)
+                        </TabsTrigger>
+
+                        <TabsTrigger
+                            value="favorites"
+                            className="flex items-center gap-2"
+                            onClick={() => setActiveTab("favorites")}
+                        >
+                            <Heart className="h-4 w-4" />
+                            Favorites (3)
+                        </TabsTrigger>
+
+                        <TabsTrigger value="heroes"
+                            onClick={() => setActiveTab("heroes")}
+                        >
+                            Heroes (12)
+                        </TabsTrigger>
+
+                        <TabsTrigger
+                            value="villains"
+                            onClick={() => setActiveTab("villains")}
+                        >
+                            Villains (2)
+                        </TabsTrigger>
+                    </TabsList>
+
+                    <TabsContent value={"all"}>
+                        {/* Mostrar todos los personajes */}
+                        <h1>Todos los personajes</h1>
+                        <HeroGrid />
+                    </TabsContent>
+
+                    <TabsContent value={"favorites"}>
+                        {/* Mostrar todos los personajes favoritos*/}
+                        <h1>Favoritos</h1>
+                        <HeroGrid />
+                    </TabsContent>
+
+                    <TabsContent value={"heroes"}>
+                        {/* Mostrar todos los heroes */}
+                        <h1>Heroes</h1>
+                        <HeroGrid />
+                    </TabsContent>
+
+                    <TabsContent value={"villains"}>
+                        {/* Mostrar todos los villanos */}
+                        <h1>Villanos</h1>
+                        <HeroGrid />
+                    </TabsContent>
+                </Tabs>
+
+                {/* Pagination */}
+                <CustomPagination totalPages={4} />
+            </>
+        </>
     )
 }
