c845ed7dc74c2746c4cd56bb0cb439f9977b4195
AntonioBT451 <antoniobt451@gmail.com>
2026-05-26 18:17:17 -0600

    feat: persist active tab state using URL search parameters with tab validation

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -1,27 +1,30 @@
 // src/heroes/pages/home/HomePage.tsx
-
-import {
-    Heart,
-} from "lucide-react"
-
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { CustomJumbotron } from '../../../components/custom/CustomJumbotron';
-import { HeroStats } from "@/heroes/components/HeroStats"
-import { HeroGrid } from "@/heroes/components/HeroGrid"
-import { useState } from "react";
-import { CustomPagination } from "@/components/custom/CustomPagination";
-import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
-import { getHeroesByPageAction } from "@/heroes/actions/get-heroes-by-page.action";
+import { useMemo } from "react";
+import { useSearchParams } from "react-router";
 import { useQuery } from "@tanstack/react-query";
+
+import { Heart } from "lucide-react"
+
+import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
+import { CustomJumbotron } from '../../../components/custom/CustomJumbotron';
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { getHeroesByPageAction } from "@/heroes/actions/get-heroes-by-page.action";
+import { HeroGrid } from "@/heroes/components/HeroGrid"
+import { HeroStats } from "@/heroes/components/HeroStats"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 
 export const HomePage = () => {
 
-    const [activeTab, setActiveTab] = useState<
-        'all' |
-        'favorites' |
-        'heroes' |
-        'villains'
-    >('all');
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const activeTab = searchParams.get('tab') ?? 'all';
+
+    const selectedTab = useMemo(() => {
+        const validTabs = ['all', 'favorites', 'heroes', 'villains'];
+
+        return validTabs.includes(activeTab) ? activeTab : 'all';
+    }, [activeTab]);
 
     const { data: heroesResponse } = useQuery({
         queryKey: ['heroes'],
@@ -53,10 +56,13 @@ export const HomePage = () => {
                 <HeroStats />
 
                 {/* Tabs */}
-                <Tabs value={activeTab} className="mb-8">
+                <Tabs value={selectedTab} className="mb-8">
                     <TabsList className="grid w-full grid-cols-4">
                         <TabsTrigger value="all"
-                            onClick={() => setActiveTab('all')}
+                            onClick={() => setSearchParams((prev) => {
+                                prev.set('tab', 'all');
+                                return prev;
+                            })}
                         >
                             All Characters (16)
                         </TabsTrigger>
@@ -64,21 +70,30 @@ export const HomePage = () => {
                         <TabsTrigger
                             value="favorites"
                             className="flex items-center gap-2"
-                            onClick={() => setActiveTab("favorites")}
+                            onClick={() => setSearchParams((prev) => {
+                                prev.set('tab', 'favorites');
+                                return prev;
+                            })}
                         >
                             <Heart className="h-4 w-4" />
                             Favorites (3)
                         </TabsTrigger>
 
                         <TabsTrigger value="heroes"
-                            onClick={() => setActiveTab("heroes")}
+                            onClick={() => setSearchParams((prev) => {
+                                prev.set('tab', 'heroes');
+                                return prev;
+                            })}
                         >
                             Heroes (12)
                         </TabsTrigger>
 
                         <TabsTrigger
                             value="villains"
-                            onClick={() => setActiveTab("villains")}
+                            onClick={() => setSearchParams((prev) => {
+                                prev.set('tab', 'villains');
+                                return prev;
+                            })}
                         >
                             Villains (2)
                         </TabsTrigger>
