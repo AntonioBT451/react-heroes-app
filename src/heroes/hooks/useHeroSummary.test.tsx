aba6b0c33eff68aaf64c118d3ca363c7a50568aa
AntonioBT451 <antoniobt451@gmail.com>
2026-07-02 20:44:43 -0600

    test: implement useHeroSummary tests with success, error, and loading states

---
+++ src/heroes/hooks/useHeroSummary.test.tsx
@@ -0,0 +1,137 @@
+// src/heroes/hooks/useHeroSummary.test.tsx
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { PropsWithChildren } from "react";
+import { describe, expect, test, vi } from "vitest"
+import { useHeroSummary } from "./useHeroSummary";
+import { renderHook, waitFor } from "@testing-library/react";
+import { getSummaryAction } from "../actions/get-summary.actions";
+import type { SummaryResponse } from "../types/get-summary.response";
+
+vi.mock('../actions/get-summary.actions', () => ({
+    getSummaryAction: vi.fn(),
+}));
+
+const mockGetSummaryAction = vi.mocked(getSummaryAction);
+
+const tanStackCustomProvider = () => {
+
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                retry: false
+            },
+        },
+    });
+
+    return ({ children }: PropsWithChildren) => (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
+
+describe('useHeroSummary', () => {
+
+    test('should return the initial state (isLoading)', () => {
+        const { result } = renderHook(() => useHeroSummary(), {
+            wrapper: tanStackCustomProvider(),
+        });
+
+        expect(result.current.isLoading).toBe(true);
+        expect(result.current.isError).toBe(false);
+        expect(result.current.data).toBe(undefined);
+        expect(result.current.data).toBeUndefined();
+    });
+
+    test('should return success state with data when API call succeeds', async () => {
+
+        const mockSummaryData = {
+            "totalHeroes": 25,
+            "strongestHero": {
+                "id": "1",
+                "name": "Clark Kent",
+                "slug": "clark-kent",
+                "alias": "Superman",
+                "powers": [
+                    "Súper fuerza",
+                    "Vuelo",
+                    "Visión de calor",
+                    "Visión de rayos X",
+                    "Invulnerabilidad",
+                    "Súper velocidad"
+                ],
+                "description": "El Último Hijo de Krypton, protector de la Tierra y símbolo de esperanza para toda la humanidad.",
+                "strength": 10,
+                "intelligence": 8,
+                "speed": 9,
+                "durability": 10,
+                "team": "Liga de la Justicia",
+                "image": "1.jpeg",
+                "firstAppearance": "1938",
+                "status": "Active",
+                "category": "Hero",
+                "universe": "DC"
+            },
+            "smartestHero": {
+                "id": "2",
+                "name": "Bruce Wayne",
+                "slug": "bruce-wayne",
+                "alias": "Batman",
+                "powers": [
+                    "Artes marciales",
+                    "Habilidades de detective",
+                    "Tecnología avanzada",
+                    "Sigilo",
+                    "Genio táctico"
+                ],
+                "description": "El Caballero Oscuro de Ciudad Gótica, que utiliza el miedo como arma contra el crimen y la corrupción.",
+                "strength": 6,
+                "intelligence": 10,
+                "speed": 6,
+                "durability": 7,
+                "team": "Liga de la Justicia",
+                "image": "2.jpeg",
+                "firstAppearance": "1939",
+                "status": "Active",
+                "category": "Hero",
+                "universe": "DC"
+            },
+            "heroCount": 18,
+            "villainCount": 7
+        } as SummaryResponse;
+
+        mockGetSummaryAction.mockResolvedValue(mockSummaryData);
+
+        const { result } = renderHook(() => useHeroSummary(), {
+            wrapper: tanStackCustomProvider(),
+        });
+
+        await waitFor(() => {
+            expect(result.current.isSuccess).toBe(true);
+        });
+
+        expect(result.current.isError).toBe(false);
+        expect(mockGetSummaryAction).toHaveBeenCalled();
+    });
+
+    test('should return error state when API call fails', async () => {
+
+        const mockError = new Error('Faild to fetch summary');
+
+        mockGetSummaryAction.mockRejectedValue(mockError);
+
+        const { result } = renderHook(() => useHeroSummary(), {
+            wrapper: tanStackCustomProvider(),
+        });
+
+        await waitFor(() => {
+            expect(result.current.isError).toBe(true);
+        });
+
+        expect(result.current.error).toBeDefined();
+        expect(result.current.isLoading).toBe(false);
+        expect(mockGetSummaryAction).toHaveBeenCalled();
+        expect(result.current.error?.message).toBe('Faild to fetch summary');
+    });
+
+});
